a78975f2ffeaff276bce0b657854a18b2d4bd709
PrutskovDmitriy <92093765+PrutskovDmitriy@users.noreply.github.com>
2021-10-27 05:15:27 +0300

    Update Homework_5_3.py

--- Homework_5_3.py
+++ Homework_5_3.py
@@ -7,5 +7,3 @@ klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 # my_zip = zip(tutors, klasses)
 # my_list = list(zip(tutors, klasses))
 # print(my_list)
-def my_gen(max_gen):
-    for i in range(1, max_gen)
