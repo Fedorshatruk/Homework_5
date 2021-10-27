b13a66fc2497ef429135821271e695d552df0e7a
PrutskovDmitriy <92093765+PrutskovDmitriy@users.noreply.github.com>
2021-10-27 05:15:58 +0300

    Update Homework_5_3.py

--- Homework_5_3.py
+++ Homework_5_3.py
@@ -1,9 +1,12 @@
 tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
 klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
-# if len(tutors) < len(klasses):
-#     tutors.append('None')
-# if len(tutors) > len(klasses):
-#     klasses.append('None')
-# my_zip = zip(tutors, klasses)
-# my_list = list(zip(tutors, klasses))
-# print(my_list)
+if len(tutors) < len(klasses):
+    tutors.append('None')
+if len(tutors) > len(klasses):
+    klasses.append('None')
+my_zip = zip(tutors, klasses)
+print(next(my_zip))
+# from itertools import zip_longest
+# my_gen = ((tutors, klasses) for tutors, klasses in zip_longest(tutors, klasses))
+# print(next(my_gen))
+
